f67b86d570ff5bc0aa2261a7d9e575b7226ad5d1
Alireza Razavi <114526330+qMRoRZp@users.noreply.github.com>
2023-02-23 12:41:28 +0330

    Update Text with number.py

--- Text with number.py
+++ Text with number.py
@@ -3,5 +3,5 @@ b = len(a)
 for i in range (b):
     if a[i]== '0' or a[i]=='1' or a[i]=='2' or a[i]=='3' or a[i]=='4' or a[i]=='5' or a[i]=='6' or a[i]=='7' or a[i]=='8' or a[i]=='9':
         print ('ERROR : <Text have number in it>')
-    else
-    print ('Text is with out number.')
+    else :
+     print ('Text is with out number.')
